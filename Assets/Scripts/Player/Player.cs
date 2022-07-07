5aebf94c55c7e6b5887d1685dd01262e5f811092
Lucas Linhares <lucas.lteccorp@gmail.com>
2022-07-07 13:27:40 -0300

    implement jump prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,18 +6,40 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
-    public float speed;
+    public float speed = 10f;
+
+    public float jumpForce = 2f;
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            /*myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);*/
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
-        } else if (Input.GetKey(KeyCode.RightArrow))
+        HandleJump();
+        HandleMovements();
+    }
+
+    private void HandleMovements()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            /*myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime)*/
+            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+        }
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        } else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+    }
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            myRigidbody.velocity = Vector2.up * jumpForce;
         }
     }
 }
